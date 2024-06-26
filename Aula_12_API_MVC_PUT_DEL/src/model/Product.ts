e705dbf594b1083cb6fd0210c5f12dd5f1626d56
EduJobs015 <edualbuq8@gmail.com>
2024-06-26 08:28:19 -0300

    att2

---
+++ Aula_12_API_MVC_PUT_DEL/src/model/Product.ts
@@ -0,0 +1,11 @@
+export class Pao{
+    private id: number;
+    private nome: string;
+    private vegano: boolean;
+}
+
+constructor (id: number, nome: string, vegano:boolean){
+    this.id = id;
+    this.nome = nome;
+    this.vegano = vegano;
+}
